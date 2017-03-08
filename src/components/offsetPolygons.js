3490cb72adeb49f44fe07947fadefb5d0f37d18d
John Rees <john@bitsushi.com>
2017-03-08 22:36:56 +0000

    offset polygons

--- src/components/offsetPolygons.js
+++ src/components/offsetPolygons.js
@@ -4,11 +4,25 @@ import { Clipper } from 'clipsy'
 // jtRound: 1,
 // jtMiter: 2
 
-const implementation = ({ POINTS, DELTA, MITERLIMIT, JOINTYPE, AUTOFIX } = {}) => {
+const implementation = ({ POINTS, DELTA, MITERLIMIT, JOINTYPE, AUTOFIX, CONVERT = true } = {}) => {
+  const isArray = Array.isArray(POINTS[0])
+  let correctedPoints = POINTS
+
+  // expects POINTS to be [{X:0,Y:0},{X:1,Y:1}...]
+
+  if (isArray) {  // if gets [[0,0],[1,1]...]
+    correctedPoints = POINTS.map(p => ({X: p[0], Y: p[1]}))
+  }
+
   let clipper = new Clipper()
-  const offsetPoints = clipper.OffsetPolygons(
-    [POINTS], DELTA, JOINTYPE, MITERLIMIT, AUTOFIX
+  let offsetPoints = clipper.OffsetPolygons(
+    [correctedPoints], DELTA, JOINTYPE, MITERLIMIT, AUTOFIX
   )[0]
+
+  if (isArray && !CONVERT) {
+    offsetPoints = offsetPoints.map(p => ([p.X, p.Y]))
+  }
+
   return { POINTS: offsetPoints }
 }
 
--- src/components/offsetPolygons.test.js
+++ src/components/offsetPolygons.test.js
@@ -10,3 +10,25 @@ it('can offsetPolygons', () => {
   }
   expect(result).toEqual(expectedResult)
 })
+
+it('converts offsetPolygons received in array format', () => {
+  const points = [[0, 0], [0, 1], [1, 1], [1, 0]]
+  const result = offsetPolygons.implementation(
+    {POINTS: points, DELTA: 5, MITERLIMIT: 10, JOINTYPE: 2, AUTOFIX: true, CONVERT: true}
+  )
+  const expectedResult = {
+    POINTS: [{X: -5, Y: 6}, {X: -5, Y: -5}, {X: 6, Y: -5}, {X: 6, Y: 6}]
+  }
+  expect(result).toEqual(expectedResult)
+})
+
+it('can offsetPolygons received in array format', () => {
+  const points = [[0, 0], [0, 1], [1, 1], [1, 0]]
+  const result = offsetPolygons.implementation(
+    {POINTS: points, DELTA: 5, MITERLIMIT: 10, JOINTYPE: 2, AUTOFIX: true, CONVERT: false}
+  )
+  const expectedResult = {
+    POINTS: [[-5, 6], [-5, -5], [ 6, -5], [ 6, 6]]
+  }
+  expect(result).toEqual(expectedResult)
+})
